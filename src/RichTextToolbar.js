e0632e38a3f85259d6d2cfcfe44b803a91ff1cc7
Yedidya Kennard <yedidyak@wix.com>
2016-11-03 09:46:20 +0200

    Basic toolbar working

--- src/RichTextToolbar.js
+++ src/RichTextToolbar.js
@@ -1,5 +1,26 @@
 import React, {Component, PropTypes} from 'react';
-import {View, TouchableOpacity} from 'react-native';
+import {View, TouchableOpacity, Text} from 'react-native';
+import {actions} from './const';
+
+const defaultActions = [
+  actions.insertImage,
+  actions.setBold,
+  actions.setItalic,
+  actions.insertBulletsList,
+  actions.insertOrderedList,
+  actions.insertLink
+];
+
+function getDefaultIconText() {
+  const texts = {};
+  texts[actions.insertImage] = 'IMG';
+  texts[actions.setBold] = 'B';
+  texts[actions.setItalic] = 'I';
+  texts[actions.insertBulletsList] = '0';
+  texts[actions.insertOrderedList] = '1';
+  texts[actions.insertLink] = '<a>';
+  return texts;
+}
 
 
 export default class RichTextToolbar extends Component {
@@ -8,15 +29,42 @@ export default class RichTextToolbar extends Component {
     getEditor: PropTypes.func.isRequired
   };
 
-  render() {
+  _getButton(action, selected) {
     return (
       <TouchableOpacity
-        style={{
-          height: 50,
-          backgroundColor: 'red'
-        }}
-        onPress={() => this.props.getEditor().setBold()}
-      />
+          key={action}
+          style={{flex: 1, backgroundColor: 'blue', justifyContent: 'center'}}
+          onPress={() => this._onPress(action)}
+      >
+        <Text style={{textAlign: 'center'}}>
+          {getDefaultIconText()[action]}
+        </Text>
+      </TouchableOpacity>
     );
+  }
+
+  render() {
+    return (
+      <View style={{flexDirection: 'row', height: 50}}>
+        {defaultActions.map((action) => this._getButton(action, false))}
+      </View>
+    );
+  }
+
+  _onPress(action) {
+    switch(action) {
+      case actions.setBold:
+        this.props.getEditor().setBold();
+        break;
+      case actions.setItalic:
+        this.props.getEditor().setItalic();
+        break;
+      case actions.insertBulletsList:
+        this.props.getEditor().insertBulletsList();
+        break;
+      case actions.insertOrderedList:
+        this.props.getEditor().insertOrderedList();
+        break;
+    }
   }
 }
