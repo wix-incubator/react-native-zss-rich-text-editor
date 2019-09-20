ce9708c683c07660a4c5ba42953f484898b7cb1c
Alex Hwang <ahwang@bigfootbiomedical.com>
2019-09-19 18:34:34 -0700

    reverse renaming for RichTextEditor and module

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import Editor from './src/RichTextEditor';
+import RichTextEditor from './src/RichTextEditor';
 import ToolBar from './src/RichTextToolbar';
 import ToolBarStyled from './src/DefaultToolbar';
 import {actions} from './src/const';
 
 module.exports = {
-  Editor,
+  RichTextEditor,
   ToolBar,
   ToolBarStyled,
   actions
