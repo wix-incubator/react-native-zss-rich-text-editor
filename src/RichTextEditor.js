8f8d21f5aca720d43e6e37ae270c618591178c0d
Elad Gelman <elad@honeybook.com>
2020-03-03 16:53:20 +0200

    fix

--- src/RichTextEditor.js
+++ src/RichTextEditor.js
@@ -356,7 +356,7 @@ export default class RichTextEditor extends Component {
 
   _sendAction(action, data) {
     let jsonString = JSON.stringify({type: action, data});
-    jsonString = this.btoa(jsonString.replace(/[\’]/g, "&apos;").replace(/'/g, "&apos;"));
+    jsonString = this.btoa(jsonString.replace(/[\’]/g, ''));
     this.webviewBridge.sendToBridge(jsonString);
   }
 
