d64e253e3784d06a7c97aa5af7bc3ebc689ec6c1
OrenMag <oren@honeybook.com>
2022-09-05 13:16:39 +0300

    more changes

--- src/RichTextEditor.js
+++ src/RichTextEditor.js
@@ -235,6 +235,11 @@ export default class RichTextEditor extends Component {
           this.props.cancelScheduleSend && this.props.cancelScheduleSend();
           break;
         }
+
+        case messages.OPEN_SCHEDULE_MENU:{
+          this.props.openScheduledSendMenu && this.props.openScheduledSendMenu();
+          break;
+        }
       }
     } catch(e) {
       //alert('NON JSON MESSAGE');
